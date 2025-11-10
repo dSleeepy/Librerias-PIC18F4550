82622d16ecb526cb8c3c00ee937a56023e2c129c
dSleepy <dsleepy666@gmail.com>
2025-11-09 23:32:49 -0500

    Add header file for ADC channel functions

---
+++ ANALOGICOS.h
@@ -0,0 +1,22 @@
+#ifndef ANALOGICOS_H
+#define	ANALOGICOS_H
+
+#include <stdint.h>
+
+void leer_adc(void);
+uint16_t CANAL0(void);
+uint16_t CANAL1(void);
+uint16_t CANAL2(void);
+uint16_t CANAL3(void);
+uint16_t CANAL4(void);
+uint16_t CANAL5(void);
+uint16_t CANAL6(void);
+uint16_t CANAL7(void);
+uint16_t CANAL8(void);
+uint16_t CANAL9(void);
+uint16_t CANAL10(void);
+uint16_t CANAL11(void);
+uint16_t CANAL12(void);
+uint16_t CANAL13(void);
+
+#endif	/* ANALOGICOS_H */
