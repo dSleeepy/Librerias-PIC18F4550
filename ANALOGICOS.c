f25dbdc4491ee58952b1e32a32411eff0fb7f101
dSleepy <dsleepy666@gmail.com>
2025-11-16 14:44:28 -0500

    Refactor ADC functions into a single channel function

--- ANALOGICOS.c
+++ ANALOGICOS.c
@@ -1,88 +1,21 @@
 #include "ANALOGICOS.h"
 #include <stdint.h>
 #include <xc.h>
+#include "delay16mhz.h"
+#define _XTAL_FREQ 16000000
 
-void leer_adc(void){
-    ADCON1bits.VCFG01=0;
-    ADCON2bits.ACQT=0b010;
-    ADCON2bits.ADCS=0b101;
+void ADC_Init(void){
+    ADCON1bits.VCFG0=0;  //0=Usa voltaje negativo del micro ; 1=Usa voltaje negativo diferente
+    ADCON1bits.VCFG1=0;  //0=Usa voltaje positivo del micro ; 1=Usa voltaje positivo diferente
+    ADCON2bits.ADFM=1;
+    ADCON2bits.ACQT=0b101;
+    ADCON2bits.ADCS=0b110;
     ADCON0bits.ADON=1;
 }
-uint16_t CANAL0(void){
-    ADCON0bits.CHS=0x00;
+uint16_t CANAL(uint8_t canal){
+    ADCON0bits.CHS=canal;
+    __delay_ms(50);
     ADCON0bits.GO_DONE=1;
     while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL1(void){
-    ADCON0bits.CHS=0x01;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL2(void){
-    ADCON0bits.CHS=0x02;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL3(void){
-    ADCON0bits.CHS=0x03;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL4(void){
-    ADCON0bits.CHS=0x04;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL5(void){
-    ADCON0bits.CHS=0x05;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL6(void){
-    ADCON0bits.CHS=0x06;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL7(void){
-    ADCON0bits.CHS=0x07;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL8(void){
-    ADCON0bits.CHS=0x08;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL9(void){
-    ADCON0bits.CHS=0x09;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL10(void){
-    ADCON0bits.CHS=0x0A;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL11(void){
-    ADCON0bits.CHS=0x0B;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
-}
-uint16_t CANAL12(void){
-    ADCON0bits.CHS=0x0C;
-    ADCON0bits.GO_DONE=1;
-    while(ADCON0bits.GO_DONE==1);
-    return (uint16_t)(ADRESL|(ADRESH<<8));
+    return (uint16_t)((ADRESH<<8)|ADRESL);
 }
