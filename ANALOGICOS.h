cc1cab259b8a3eec3dda5cdfe07d0834878a05f0
dSleepy <dsleepy666@gmail.com>
2025-11-16 14:44:50 -0500

    Refactor ADC functions in ANALOGICOS.h
    Replaced multiple channel functions with a single parameterized function.

--- ANALOGICOS.h
+++ ANALOGICOS.h
@@ -3,20 +3,6 @@
 
 #include <stdint.h>
 
-void leer_adc(void);
-uint16_t CANAL0(void);
-uint16_t CANAL1(void);
-uint16_t CANAL2(void);
-uint16_t CANAL3(void);
-uint16_t CANAL4(void);
-uint16_t CANAL5(void);
-uint16_t CANAL6(void);
-uint16_t CANAL7(void);
-uint16_t CANAL8(void);
-uint16_t CANAL9(void);
-uint16_t CANAL10(void);
-uint16_t CANAL11(void);
-uint16_t CANAL12(void);
-uint16_t CANAL13(void);
-
+void ADC_Init(void);
+uint16_t CANAL(uint8_t canal);
 #endif	/* ANALOGICOS_H */
